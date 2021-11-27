bd147c606f23d7d68d072325ddfd3adb4f7045c5
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-11-27 07:41:22 +0000

    Create keyword,variable.cpp

---
+++ keyword,variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double num1=10.2,num2=30.7;
+    char ch;
+    ch='a';
+    cout<<"Number1="<<num1 <<endl <<"Number2="<<num2 <<endl;
+    cout<<ch;
+    getch();
+}
