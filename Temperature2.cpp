e7ac1c1e30120aecd15801188b3f3e2b0600d743
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-11-28 18:33:07 +0000

    Create Temperature2.cpp

---
+++ Temperature2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double kel,cels;
+    cout<<"Enter kelvin:";
+    cin>>kel;
+
+    kel=cels+273;
+    cout<<"Enter kelvin="<<kel;
+    getch();
+}
