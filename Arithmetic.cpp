a61c0167c405649a0d6688bc7b3c67904e4f4f39
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-11-28 16:03:56 +0000

    Create Arithmetic.cpp

---
+++ Arithmetic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    num1=23;
+    num2=45;
+    int sum=num1+num2;
+    cout<<"sum is:"<<sum;
+    cout<<endl;
+
+    int sub=num1-num2;
+    cout<<"subtrsction is:"<<sub;
+    cout<<endl;
+    getch();
+}
