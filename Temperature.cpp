40b802198370c1b1aa167e3abf4cca35e312ede3
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-11-28 18:24:20 +0000

    Create Temperature.cpp

---
+++ Temperature.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double farn,cels;
+    cout<<"Enter celsius:";
+    cin>>cels;
+
+    farn=1.8*cels+32;
+    cout<<"Enter fahrenheit=";
+    getch();
+}
