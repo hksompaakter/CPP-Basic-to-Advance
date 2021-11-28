fa25bc6b6b435e11b5f696e068075652256c793c
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-11-28 18:47:02 +0000

    Create operator.cpp

---
+++ operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int x=3;
+
+    x+=2;
+    cout<<x<<endl;
+    x-=1;
+    cout<<x<<endl;
+    x*=2;
+    cout<<x<<endl;
+    getch();
+}
