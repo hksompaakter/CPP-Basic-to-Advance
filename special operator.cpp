5b57b74b2c9485fd70188b0bb22e3a4dfa3898ac
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-11-29 16:10:56 +0000

    Create special operator.cpp

---
+++ special operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    double d;
+    float f;
+    char ch;
+    char name[10];
+
+    int c=sizeof(name);
+
+    cout<<c;
+    getch();
+}
