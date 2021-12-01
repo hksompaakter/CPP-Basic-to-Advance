1ba500e3b0966b2218a2eaba4e0d03801bdbcb06
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-12-01 06:38:32 +0000

    Create Ternary.cpp

---
+++ Ternary.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1,num2;
+
+    num1=20;
+    num2=40;
+
+    int max=(num1>num2)? num1:num2;
+    cout<<max;
+    getch();
+}
