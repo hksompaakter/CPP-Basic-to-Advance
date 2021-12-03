9a5c6b9ebae34b6026cee03ac59156690b5405a7
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-12-03 06:35:49 +0000

    Create while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int i=1;
+    while(i<=20){
+            cout<<i<<endl;
+            i++;
+
+
+    }
+
+    getch();
+}
