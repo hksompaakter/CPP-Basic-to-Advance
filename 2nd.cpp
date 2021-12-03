b7c39952b4669d379816042884f1a2a5497ae376
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-12-03 06:34:57 +0000

    Create 2nd.cpp

---
+++ 2nd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int i=2;
+    while(i<=20){
+            cout<<i<<endl;
+            i=i+2;
+
+
+    }
+
+    getch();
+}
