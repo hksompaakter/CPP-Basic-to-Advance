711e1629ae4ad5d3cb3532dc8a2553f9a0ac9e3a
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-12-03 07:17:14 +0000

    Create Do while.cpp

---
+++ Do while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int  i=1;
+    do{
+        cout<<"Bangladesh"<<endl;
+        i++;
+    }while(i<=5);
+        cout<<"End of the loop";
+
+    getch();
+}
