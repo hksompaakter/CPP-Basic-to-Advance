834f9883af646e5cdc09abc358be5a9afd3e78cc
Sompa Akter <84368639+hksompaakter@users.noreply.github.com>
2021-12-12 05:01:53 +0000

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int marks [5]={23,45,67,43,23};
+    cout<<marks[0]<<" ";
+    cout<<marks[1]<<" ";
+
+    cout<<marks[2]<<" ";
+
+    cout<<marks[3]<<" ";
+
+    cout<<marks[4]<<" ";
+
+
+    getch();
+}
